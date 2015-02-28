40cd6525cc090c04af20693f0718047c9819f53c
Jerry Hu <ferrishu3886@gmail.com>
2015-02-28 15:16:13 -0500

    Add script to display the objectives in the game

---
+++ HW5/Assets/Scripts/ObjectivesController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ObjectivesController : MonoBehaviour
+{
+
+		public Text obj;
+		GameObject[] scientists;
+		int numLeft;
+
+		// Use this for initialization
+		void Start ()
+		{
+				
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				scientists = GameObject.FindGameObjectsWithTag ("objectives");
+				numLeft = scientists.Length;
+				//Debug.Log ("numLeft: " + numLeft);
+				obj.GetComponent<Text> ().text = "Scientists left: " + numLeft;
+				
+		}
+}
