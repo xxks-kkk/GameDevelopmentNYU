13112cda939c3a2885a84de884c10031aa23d34b
Jerry Hu <ferrishu3886@gmail.com>
2015-02-27 20:50:54 -0500

    Don't allow player to do multiple jumps

--- HW5/Assets/Scripts/PlayerController.cs
+++ HW5/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,15 @@ public class PlayerController : MonoBehaviour
 {
 		public float speed = 15f;
 		Rigidbody rbody;
+		float distToGround;
 
 		// Use this for initialization
 		void Start ()
 		{
 				rbody = GetComponent<Rigidbody> ();
+
+				// get the distance to ground
+				distToGround = collider.bounds.extents.y;
 		}
 	
 		// Update is called once per frame
@@ -22,8 +26,14 @@ public class PlayerController : MonoBehaviour
 		{
 				rbody.AddForce (transform.forward * speed * Input.GetAxis ("Vertical"));
 				rbody.AddForce (transform.right * speed * Input.GetAxis ("Horizontal"));
-				if (Input.GetKeyDown (KeyCode.Space)) {
+				if (Input.GetKeyDown (KeyCode.Space) && isGrounded ()) {
 						rbody.AddForce (transform.up * 10, ForceMode.Impulse);
 				}
 		}
+		
+		bool isGrounded ()
+		{
+				return Physics.Raycast (transform.position, -Vector3.up, distToGround + 0.1f);
+		}
+		
 }
