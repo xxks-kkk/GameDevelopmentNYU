c2007d13e0ee9b7adeeb50fb7a6463c6154ca837
Jerry Hu <ferrishu3886@gmail.com>
2015-02-27 22:36:30 -0500

    Add a simple timer to the scene

---
+++ HW5/Assets/Scripts/TimerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+
+		float timer = 200.0f;
+		public Text time;
+	
+
+		// Update is called once per frame
+		void Update ()
+		{
+				timer -= Time.deltaTime;
+
+				if (timer <= 0) {
+						timer = 0;
+				}
+				time.GetComponent<Text> ().text = timer.ToString ("0");
+		}
+}
