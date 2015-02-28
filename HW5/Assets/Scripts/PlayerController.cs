057c56953832603e16664e5324be06ac4d4c1d68
Jerry Hu <ferrishu3886@gmail.com>
2015-02-27 19:57:01 -0500

    Finish first person player controller using rigidbody

---
+++ HW5/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+		public float speed = 15f;
+		Rigidbody rbody;
+
+		// Use this for initialization
+		void Start ()
+		{
+				rbody = GetComponent<Rigidbody> ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void FixedUpdate ()
+		{
+				rbody.AddForce (transform.forward * speed * Input.GetAxis ("Vertical"));
+				rbody.AddForce (transform.right * speed * Input.GetAxis ("Horizontal"));
+				if (Input.GetKeyDown (KeyCode.Space)) {
+						rbody.AddForce (transform.up * 10, ForceMode.Impulse);
+				}
+		}
+}
