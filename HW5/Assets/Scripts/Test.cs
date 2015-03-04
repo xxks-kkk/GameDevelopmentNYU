aeffeb925b5d4c878f3141b0110c2521597b4e17
Jerry Hu <ferrishu3886@gmail.com>
2015-03-03 19:58:07 -0500

    Add Test object to canvas

---
+++ HW5/Assets/Scripts/Test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+		public Text test;
+		GameObject GM;
+
+		// Use this for initialization
+		void Start ()
+		{
+				GM = GameObject.Find ("GameManager");
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				string buffer = "";
+				buffer = GM.GetComponent<GameManager> ().insideFacility.ToString ();
+				
+				test.GetComponent<Text> ().text = buffer;
+		}
+}
