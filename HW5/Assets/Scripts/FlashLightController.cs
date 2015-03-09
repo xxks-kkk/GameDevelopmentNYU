0ea0a998c740c5b4988bb65d9fcb19e78a2a18e6
Jerry Hu <ferrishu3886@gmail.com>
2015-03-09 01:42:44 -0400

    Now, the torchlight will be off when the power runs out

--- HW5/Assets/Scripts/FlashLightController.cs
+++ HW5/Assets/Scripts/FlashLightController.cs
@@ -1,26 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class FlashLightController : MonoBehaviour
 {
 
 		public bool FlashLightOn = false;
+		Slider PowerBarSlider;
 
 		// Use this for initialization
 		void Start ()
 		{
-	
+				GameObject tmp = GameObject.Find ("PowerBarSlider");
+				PowerBarSlider = tmp.GetComponent<Slider> ();
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
-				if (Input.GetButtonDown ("toggleLight") && FlashLightOn == false) {
-						light.intensity = 3;
-						FlashLightOn = true;
-				} else if (Input.GetButtonDown ("toggleLight") && FlashLightOn) {
-						light.intensity = 0;
+				if (PowerBarSlider.value > 0) {
+						if (Input.GetButtonDown ("toggleLight") && FlashLightOn == false) {
+								light.intensity = 3;
+								FlashLightOn = true;
+						} else if (Input.GetButtonDown ("toggleLight") && FlashLightOn) {
+								light.intensity = 0;
+								FlashLightOn = false;
+						}
+				} else {
 						FlashLightOn = false;
+						light.intensity = 0;
 				}
 		}
 }
