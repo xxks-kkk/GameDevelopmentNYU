f10098f3e18bb6e32ad8d147ceb093e8d863bf9f
Jerry Hu <ferrishu3886@gmail.com>
2015-03-09 00:36:04 -0400

    Add flashlight controller

---
+++ HW5/Assets/Scripts/FlashLightController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashLightController : MonoBehaviour
+{
+
+		public bool FlashLightOn = false;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Input.GetButtonDown ("toggleLight") && FlashLightOn == false) {
+						light.intensity = 2;
+						FlashLightOn = true;
+				} else if (Input.GetButtonDown ("toggleLight") && FlashLightOn) {
+						light.intensity = 0;
+						FlashLightOn = false;
+				}
+		}
+}
