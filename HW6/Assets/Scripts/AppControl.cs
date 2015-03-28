f44ffedb268f6fb42b588389c536baec98dc3e57
Jerry Hu <ferrishu3886@gmail.com>
2015-03-27 23:28:34 -0400

    Finish AppControl script

--- HW6/Assets/Scripts/AppControl.cs
+++ HW6/Assets/Scripts/AppControl.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AppControl : MonoBehaviour {
+public class AppControl : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+		// Use this for initialization
+		void Start ()
+		{
 	
-	}
+		}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+		// Update is called once per frame
+		void Update ()
+		{
+				// did the player press [R] on the keyboard?
+				if (Input.GetKeyDown (KeyCode.R)) {
+						// if so, reload the current scene
+						Application.LoadLevel (Application.loadedLevel);
+				}
+		}
 }
