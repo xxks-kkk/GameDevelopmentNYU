92c2584bb2b795d7ebdd9b8e1133de47a7620622
Jerry Hu <ferrishu3886@gmail.com>
2015-03-27 23:07:01 -0400

    Add GridInstantiate.cs (Not sure if I have implemented correctly)

---
+++ HW6/Assets/Scripts/GridInstantiate.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// put this on a small cube, it will instantiate a grid of 5x5 floors with random walls
+public class GridInstantiate : MonoBehaviour
+{
+		public Transform floorPrefab;
+		public Transform wallPrefab;
+		float rand;
+
+		// Use this for initialization
+		void Start ()
+		{
+				for (int x = 0; x < 5; x++) {
+						for (int z = 0; z<5; z++) {
+								Vector3 pos = new Vector3 (x * 0.5f, 0, z * 0.5f) + transform.position;
+								rand = Random.Range (0f, 1f);
+								//Debug.Log ("Pos: " + pos);
+								//Debug.Log ("Cur pos: " + transform.position);
+								//Debug.Log ("rand: " + rand);
+								if (rand <= 0.7f) {
+										Instantiate (floorPrefab, pos, Quaternion.identity);
+								} else if (rand < 0.95f) {
+										Instantiate (wallPrefab, pos, Quaternion.identity);
+								} else {
+
+								}
+						}
+				}
+				Destroy (gameObject);
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
