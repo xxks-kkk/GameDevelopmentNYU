dd5590b54aea39b3759f1f2aef858af28d8d77a7
Jerry Hu <ferrishu3886@gmail.com>
2015-04-05 18:02:22 -0400

    Setup project

---
+++ HW7/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+		Ray ray;
+		RaycastHit rayHit;
+		Vector3 movDir;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				// move with the right click
+				if (Input.GetMouseButtonDown (1)) {
+						ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+						rayHit = new RaycastHit ();
+						Physics.Raycast (ray, out rayHit, 1000f);
+				}
+
+				movDir = rayHit.point - transform.position;
+				
+		}
+
+		void FixedUpdate ()
+		{
+				GetComponent<Rigidbody> ().AddForce (movDir * 10);
+		}
+}
