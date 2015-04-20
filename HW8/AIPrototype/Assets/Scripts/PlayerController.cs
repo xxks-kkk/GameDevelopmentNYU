c42682fd769101292b75b0b94e02af3ab0e4de2f
Jerry Hu <ferrishu3886@gmail.com>
2015-04-19 22:45:36 -0400

    Add simple player control for AI testing purpose

---
+++ HW8/AIPrototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+	float speed = 5f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += transform.forward * Time.deltaTime * speed;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position -= transform.forward * Time.deltaTime * speed;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (0f, -90f * Time.deltaTime, 0f);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			// when you hardcode a constant value, we call that a "magic number"
+			transform.Rotate (0f, 90f * Time.deltaTime, 0f);
+		}
+	}
+}
